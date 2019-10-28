5e3d5b6beb9e8a215c88ff6582c3dce0eb6d744d
Octavio Elsacte Palma <oelsacte@gmail.com>
2019-10-28 00:15:07 -0600

    Flux essencial

---
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,51 @@
+package com.bolsadeideas.springboot.reactor.app;
+
+import javax.management.RuntimeErrorException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.bolsadeideas.springboot.reactor.app.models.Usuario;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Flux<String> nombres = Flux.just("Andres Guzman", "Pedro Fulano", "María Fulana", "Diego Sultano",
+				"Juan Mengano", "Bruce Lee", "Bruce Willis");
+		
+		Flux<Usuario> usuarios = nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> usuario.getNombre().equalsIgnoreCase("bruce")).doOnNext(usuario -> {
+					if (usuario == null) {
+						throw new RuntimeException("Nombres no pueden ser vacíos");
+					}
+					System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
+				}).map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;
+				});
+
+		usuarios.subscribe(usuario -> log.info(usuario.toString()), error -> log.error(error.getMessage()),
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha terminado la ejecución del observable");
+					}
+				});
+	}
+
+}
